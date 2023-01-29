89c0ebf11e27f37aa1410f05e17a3e08957d664e
Errytagedesign <errytage@gmail.com>
2023-01-29 17:36:36 +0100

    file structures

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import { useEffect } from "react";
+
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "aos/dist/aos.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const AOS = require("aos");
+  useEffect(() => {
+    AOS.init();
+  }, [AOS]);
+
+  return <Component {...pageProps} />;
 }
